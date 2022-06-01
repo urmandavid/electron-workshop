48ac9b90140b0926690ea805a4baf1aee7f756f8
urmandavid <davurm0211@skola.goteborg.se>
2022-06-01 10:59:59 +0200

    small fix

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from "vue-router"
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-2 vh-100 d-flex flex-column align-items-center bg- gray-200 divider-right">
-				<RouterLink to="/" class="btn btn-secondary w-75 my- 2">Home</RouterLink>
+				<RouterLink to="/" class="btn btn-secondary w-75 mb-1 my- 2">Home</RouterLink>
 
 				<RouterLink to="/about" class="btn btn-secondary w-75 my- 2">About</RouterLink>
 			</div>
